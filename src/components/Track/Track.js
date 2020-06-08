db23c3bb13165d980fb4e7aaa91ffc7dd0bc9273
Brent Gorwin <brent.gorwin@gmail.com>
2020-06-07 20:44:34 -0500

    Added the state to App.js and hard coded the searchResults objects. Passed them through App, SearchResults, TrackList and Playlist components

--- src/components/Track/Track.js
+++ src/components/Track/Track.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
+import './Track.css';
 
 export default class Track extends Component {
+	renderAction() {
+		if (this.props.isRevoval) {
+			return <button className="Track-action">-</button>;
+		} else {
+			return <button className="Track-action">+</button>;
+		}
+	}
 	render() {
 		return (
 			<div className="Track">
 				<div className="Track-information">
-					{/* <h3><!-- track name will go here --></h3> */}
-					{/* <p><!-- track artist will go here--> | <!-- track album will go here --></p> */}
+					<h3>{this.props.track.name}</h3>
+					<p>
+						{this.props.track.artist} | {this.props.track.album}
+					</p>
 				</div>
-				{/* <button className="Track-action"><!-- + or - will go here --></button> */}
+				{this.renderAction()}
 			</div>
 		);
 	}
--- src/components/TrackList/TrackList.js
+++ src/components/TrackList/TrackList.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import Track from '../Track/Track';
+import './TrackList.css';
 
 export default class TrackList extends Component {
 	render() {
 		return (
 			<div className="TrackList">
-				{/* <!-- You will add a map method that renders a set of Track components  --> */}
+				{this.props.tracks.map((track) => {
+					return <Track track={track} key={track.id} />;
+				})}
 			</div>
 		);
 	}
